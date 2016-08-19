84e26b5e3601f60528cb2c6e7718c42924c3f6ca
Abigail <acdommer@gmail.com>
2016-08-18 17:04:56 -0700

    removing everything

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  has_secure_password
-  EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :first_name, :last_name, :email, presence:true
-  validates :email, uniqueness: {case_sensitive: false}, format:{with: EMAIL_REGEX}
-  validates :password, length: {minimum: 6}
-end
